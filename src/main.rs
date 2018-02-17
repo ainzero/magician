c59ac44a56c88f86f86aab153c10699b2b5830db
Anthony Inzero <inzero@protonmail.com>
2018-02-16 19:02:33 -0500

    Began process in learning to render a triangle

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,35 @@
+extern crate vulkano;
+extern crate vulkano_win;
 extern crate winit;
 
-use winit::{EventsLoop,WindowBuilder};
+use winit::{EventsLoop, WindowBuilder};
+use vulkano::instance::Instance;
 
 fn main() {
+
+    // Vulkan setup
+
+    let instance = {
+        // Ask for a list of Vulkan extensions to draw the window
+        let extensions = vulkano_win::required_extensions();
+
+        Instance::new(None, &extensions, None).expect("Could not create Vulkan instance!")
+    };
+
+    // Choose physical device to use
+
+    // TODO(Tony): do some filtering to prevent using a device that can't draw to our
+    // surface, or a device that doesn't support all the extensions we need
+
+    // TODO(Tony): Check out rust thread stuff, specifically ARC
+
+    let physical = vulkano::instance::PhysicalDevice::enumerate(&instance)
+                            .next().expect("No device is available to draw!");
+
+    println!("Using device: {} (type: {:?})", physical.name(), physical.ty());
+
+
+    // Setup Window
     let mut event_loop = EventsLoop::new();
 
     let window = WindowBuilder::new()
@@ -23,9 +50,5 @@ fn main() {
             }
         });
     }
-
-
-
-
+    
 }
-
